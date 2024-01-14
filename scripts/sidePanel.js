a00c805ef76a093c055e8e517f5b5afd6c8eb2d5
Aebel Shajan <aebelshajan@gmail.com>
2024-01-14 21:46:59 +0000

    Added drop down menu and populated it with voices from window.speechSynthesis, uhh i think i forgot to commit this

--- scripts/sidePanel.js
+++ scripts/sidePanel.js
@@ -4,37 +4,41 @@ const voiceSelect = document.querySelector("select");
 let voices = []
 
 function populateVoiceList() {
-  voices = synth.getVoices().sort(function (a, b) {
-    const aname = a.name.toUpperCase();
-    const bname = b.name.toUpperCase();
+	voices = synth.getVoices().sort(function (a, b) {
+		const aname = a.name.toUpperCase();
+		const bname = b.name.toUpperCase();
 
-    if (aname < bname) {
-      return -1;
-    } else if (aname == bname) {
-      return 0;
-    } else {
-      return +1;
-    }
-  });
-  const selectedIndex =
-    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-  voiceSelect.innerHTML = "";
+		if (aname.includes("ENGLISH")) { // INGERLANSD LES GOOO
+			return -1;
+		} else if (bname.includes("ENGLISH")) {
+			return +1;
+		} else if (aname < bname) {
+			return -1;
+		} else if (aname == bname) {
+			return 0;
+		} else {
+			return +1;
+		}
+	});
+	const selectedIndex =
+		voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+	voiceSelect.innerHTML = "";
 
-  for (let i = 0; i < voices.length; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+	for (let i = 0; i < voices.length; i++) {
+		const option = document.createElement("option");
+		option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-    if (voices[i].default) {
-      option.textContent += " -- DEFAULT";
-    }
+		if (voices[i].default) {
+			option.textContent += " -- DEFAULT";
+		}
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option);
-  }
-  voiceSelect.selectedIndex = selectedIndex;
+		option.setAttribute("data-lang", voices[i].lang);
+		option.setAttribute("data-name", voices[i].name);
+		voiceSelect.appendChild(option);
+	}
+	voiceSelect.selectedIndex = selectedIndex;
 }
 
-synth.onvoiceschanged = function() {
+synth.onvoiceschanged = function () {
 	populateVoiceList();
 };
