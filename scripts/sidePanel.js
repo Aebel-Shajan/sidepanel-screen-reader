fb0bb32ad72cc10377c14d3ec4e25e52a78cf3e5
Aebel Shajan <aebelshajan@gmail.com>
2024-01-14 21:38:41 +0000

    Added drop down menu and populated it with voices from window.speechSynthesis

---
+++ scripts/sidePanel.js
@@ -0,0 +1,40 @@
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+
+let voices = []
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase();
+    const bname = b.name.toUpperCase();
+
+    if (aname < bname) {
+      return -1;
+    } else if (aname == bname) {
+      return 0;
+    } else {
+      return +1;
+    }
+  });
+  const selectedIndex =
+    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = "";
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " -- DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+synth.onvoiceschanged = function() {
+	populateVoiceList();
+};
