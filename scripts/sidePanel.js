fb84554ec04fbd11a35b3c7bda873d74ecab891f
Aebel Shajan <aebelshajan@gmail.com>
2024-01-16 19:12:21 +0000

    added ability to click text to scrub to it

--- scripts/sidePanel.js
+++ scripts/sidePanel.js
@@ -1,12 +1,12 @@
 import * as utils from "./utils.js"
 import * as tts from "./tts.js"
 import EasySpeech from "./node_modules/easy-speech/dist/EasySpeech.js"
-import * as highlights from "./highlights.js";
 await EasySpeech.init() // required
 
 const elements = {
 	previewTextContainer: document.querySelector("#preview-text-container"),
 	previewText: document.querySelector("#preview-text"),
+	displayText: document.querySelector("#display-text"),
 	clearContents: document.querySelector("#clear-contents"),
 	playPause: document.querySelector("#play-pause-toggle"),
 	stopButton: document.querySelector("#stop"),
@@ -17,15 +17,13 @@ let voices = tts.populateVoiceList(elements.voiceSelect, EasySpeech.voices());
 let selectedVoice = voices[0];
 let currentSentenceIndex = 0;
 let state = "stopped"
-
-highlights.setup(elements.previewTextContainer);
+let currentSpeech = null;
 
 elements.clearContents.addEventListener("click", () => {
 	elements.previewText.value = "";
-	highlights.unhighlightText(elements.previewTextContainer);
 })
 
-elements.playPause.addEventListener("click", () => {
+elements.playPause.addEventListener("click", async () => {
 	switch (state) {
 		case "paused":
 			resumeSpeak();
@@ -42,8 +40,8 @@ elements.playPause.addEventListener("click", () => {
 	}
 });
 
-elements.stopButton.addEventListener("click", () => {
-	stopSpeak();
+elements.stopButton.addEventListener("click", async () => {
+	await stopSpeak();
 });
 
 elements.voiceSelect.addEventListener("change", () => {
@@ -65,71 +63,94 @@ async function startSpeak(sentenceStartIndex) {
 		tempDiv.textContent = str;
 		return tempDiv.innerHTML;
 	}
-
 	let sentences = utils.splitTextIntoSentences(encodeHTML(elements.previewText.value), 20);
-	console.log(sentences);
-	let totalCharLength = 0;
-	if (sentences.length && sentenceStartIndex < sentences.length) {
-		utils.setButtonState(elements.playPause, "pause");
-		elements.previewText.setAttribute("disabled", "disabled");
-		elements.clearContents.setAttribute("disabled", "disabled");
-		try {
-			state = "playing"
-			for (let i = 0; i < sentenceStartIndex; i++) {
-				totalCharLength += sentences[i].length;
-			}
+	try {
+		if (sentences.length && sentenceStartIndex < sentences.length) {
+			state = "playing";
+			uiHandler()
 			for (let sentenceIndex = sentenceStartIndex; sentenceIndex < sentences.length; sentenceIndex++) {
 				currentSentenceIndex = sentenceIndex;
 				const spokenSentence = sentences[sentenceIndex];
-				selectPreviewText(totalCharLength, totalCharLength + spokenSentence.length);
-				totalCharLength += spokenSentence.length;
-				await EasySpeech.speak({
+				showDisplayText(sentences, sentenceIndex);
+				currentSpeech = await EasySpeech.speak({
 					text: spokenSentence,
 					voice: selectedVoice, // optional, will use a default or fallback
 					pitch: 1,
 					rate: 1,
-					volume: 1
+					volume: 1,
 				})
+
 			}
-		} catch {}
-		if (state !== "paused") {
-			stopSpeak();
+			if (state !== "paused") {
+				stopSpeak();
+			}
 		}
+	} catch {
+
 	}
 }
 
-function selectPreviewText(startIndex, endIndex) {
-	highlights.highlightText(elements.previewTextContainer, startIndex, endIndex);
-	elements.previewText.removeAttribute("disabled");
-	// move to start pos
-	elements.previewText.selectionStart = elements.previewText.selectionEnd = startIndex;
-	elements.previewText.blur();
-	elements.previewText.focus();
-	// Disable the textarea again
-	elements.previewText.setAttribute("disabled", "disabled");
-}
-
-function pauseSpeak() {
+async function pauseSpeak() {
 	state = "paused"
+	uiHandler()
 	console.log("paused")
-	utils.setButtonState(elements.playPause, "play");
-	EasySpeech.cancel();
+	await EasySpeech.cancel();
 }
-function resumeSpeak() {
+async function resumeSpeak() {
 	state = "playing"
-	console.log(currentSentenceIndex)
+	uiHandler()
 	console.log("resumed");
-	utils.setButtonState(elements.playPause, "pause");
-	startSpeak(currentSentenceIndex);
+	await startSpeak(currentSentenceIndex);
 }
 
-function stopSpeak() {
+async function stopSpeak() {
 	state = "stopped"
+	uiHandler()
 	console.log("error/ended")
-	utils.setButtonState(elements.playPause, "play");
-	elements.previewText.removeAttribute("disabled");
-	elements.clearContents.removeAttribute("disabled");
-	highlights.unhighlightText(elements.previewTextContainer);
 	currentSentenceIndex = 0;
-	EasySpeech.cancel();
+	await EasySpeech.cancel();
+}
+
+function showDisplayText(sentences, sentenceIndex) {
+	elements.displayText.innerHTML = "";
+	for (let j = 0; j < sentences.length; j++) {
+		let sentenceDiv = document.createElement("a");
+		let className = "sentence-div ";
+		sentenceDiv.innerHTML = sentences[j];
+		if (sentenceIndex === j) {
+			className += "current-sentence";
+		}
+		sentenceDiv.className = className;
+		sentenceDiv.addEventListener("click", async () => {
+			stopSpeak();
+			await startSpeak(j);
+
+		})
+		elements.displayText.appendChild(sentenceDiv);
+	}
+}
+
+
+function uiHandler() {
+	switch (state) {
+		case "playing":
+			utils.setButtonState(elements.playPause, "pause");
+			elements.clearContents.setAttribute("disabled", "disabled");
+			elements.previewText.style.display = "none";
+			elements.displayText.style.display = "unset";
+			elements.previewTextContainer.style.backgroundColor = "#ffffff77"
+			break;
+		case "paused":
+			utils.setButtonState(elements.playPause, "play");
+			break;
+		case "stopped":
+		default:
+			utils.setButtonState(elements.playPause, "play");
+			elements.clearContents.removeAttribute("disabled");
+			elements.previewText.style.display = "unset";
+			elements.displayText.style.display = "none";
+			elements.previewTextContainer.style.backgroundColor = "transparent"
+
+			break;
+	}
 }
